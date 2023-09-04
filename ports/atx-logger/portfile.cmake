57aba250dd1cb40ca3a952f9107aee04a3ed9472
Luigi Di Ruggiero <luigi.diruggiero@aritex-es.com>
2023-09-05 00:21:24 +0200

    cambio nome REPO

--- ports/atx-logger/portfile.cmake
+++ ports/atx-logger/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO https://github.com/Luidirug3/atx-logger
+  REPO Luidirug3/atx-logger
   REF 8844e412e1f2d91e041ca673723d4e1273fc1729
   SHA512 32a0404d5901926e2b550ebf0fc4e9d527c407fc5e929d2482cac50a6efbf19cbb309b7736426a7fd4e8ee63726743b0659cfab1780c9921a33c1e5cb79db0eb
   HEAD_REF main
